5dbd93479035053b7b5a3295462af46ff1236866
Diego R. Wille C <wille.diegoricardo@gmail.com>
2022-12-19 12:38:45 -0300

    ajustes docker files

--- frontend/js.js
+++ frontend/js.js
@@ -5,7 +5,7 @@ $("#button-blue").on("click", function() {
     var txt_comentario = $("#comment").val();
 
     $.ajax({
-        url: "http://192.168.59.100:30503",
+        url: "http://192.168.59.100:30554",
         
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
