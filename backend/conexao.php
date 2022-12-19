59ec388d3b1cf64666adaf748219bd8f9945fa88
Diego R. Wille C <wille.diegoricardo@gmail.com>
2022-12-19 11:46:45 -0300

    ajustes docker image e deploy

--- backend/conexao.php
+++ backend/conexao.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "";
+$servername = "mysql-connection";
 $username = "root";
-$password = "Senha123";
-$database = "meubanco";
+$password = "projeto_123";
+$database = "comentarios";
 
 // Criar conexão
 
